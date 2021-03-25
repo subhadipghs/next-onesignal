cb9cb4ef68c0e12827fb68004f92cc4a2a939632
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2021-03-25 09:32:13 +0530

    Added new button styles and generate random id

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
+import React from "react";
 import styles from "../styles/home.module.css";
+import { v4 } from "uuid";
 
 export default function Home() {
+  const [id, setId] = React.useState("");
   const subscribe = () => {
-    const ID = "someuser_id";
+    const ID = v4();
+    setId(ID);
     if (window) {
       console.log("Running on browser");
       OneSignal.push(() => {
-        console.log(ID);
+        console.log("Setting external id ", ID);
         OneSignal.setExternalUserId(ID);
       });
     }
   };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.center}>
       <button className={styles.button} onClick={subscribe}>
-        Say yes to Onesignal
+        Set yourself in Onesignal
       </button>
+      <p>Your external id: {id}</p>
     </div>
   );
 }
